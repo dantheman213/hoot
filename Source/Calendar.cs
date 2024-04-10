65239b88a45edc0f5845746fab211ebdd4d17171
Dan Gillespie <daniel.gillespie0949@gmail.com>
2024-04-09 17:13:04 -0700

    removed timer checks and just execue every few seconds as that provides a better result if the computer has gone to sleep and rebooted

---
+++ Source/Calendar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Google.Apis;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+namespace Hoot
+{
+    internal class Calendar
+    {
+       public async void Connect()
+        {
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                ApplicationName = "Calendar",
+                ApiKey = "[YOUR_API_KEY_HERE]",
+            });
+
+            var result = await service.Calendars.GetAsync();
+        }
+    }
+}
--- SystemTray.cs
+++ SystemTray.cs
@@ -14,6 +14,7 @@ namespace Hoot
         private NotifyIcon tray;
         private Timer timer;
         private SpeechSynthesizer synth;
+        private DateTime lastAlert;
 
         public SystemTray()
         {
@@ -36,46 +37,23 @@ namespace Hoot
             synth = new SpeechSynthesizer();
             synth.SetOutputToDefaultAudioDevice();
 
-            var nextIntervalTimeInSecs = calculateNextInterval();
-            Console.WriteLine("Timer will execute in {0} seconds or approx {1} minutes", nextIntervalTimeInSecs, (nextIntervalTimeInSecs / 60));
-
             timer = new Timer();
-            timer.Interval = (nextIntervalTimeInSecs * 1000);
+            timer.Interval = (5000); // run every 5 seconds
             timer.Tick += new EventHandler(Tick);
             timer.Start();
         }
 
-        // From when the app starts find the next top of the hour or 30 min mark in which to fire off the alert.
-        // e.g. App starts at 9:04am then this function will return 9:30am.
-        // e.g. App starts at 12:46pm then this function will return 1:00pm.
-        private int calculateNextInterval()
-        {
-            var now = DateTime.Now;
-            var minutesLeftTilInterval = 0;
-
-            if (now.Minute >= 30)
-            {
-                minutesLeftTilInterval = 60 - now.Minute;
-            }
-            else
-            {
-                minutesLeftTilInterval = 30 - now.Minute;
-            }
-
-            var nextIntervalDateTime = now.AddMinutes(minutesLeftTilInterval);
-            nextIntervalDateTime = new DateTime(nextIntervalDateTime.Year, nextIntervalDateTime.Month, nextIntervalDateTime.Day, nextIntervalDateTime.Hour, nextIntervalDateTime.Minute, 0);
-            return Convert.ToInt32((nextIntervalDateTime - now).TotalSeconds);
-        }
-
         private void Tick(object sender, EventArgs e)
         {
-            var p = new Popup();
-            p.Show();
+            var now = DateTime.Now;
+            if ((now.Minute == 0 || now.Minute == 30) && (lastAlert - now).TotalMinutes > 5)
+            {
+                lastAlert = now;
+                var p = new Popup();
+                p.Show();
 
-            synth.Speak(String.Format("The time is {0}", DateTime.Now.ToString("h:mm tt")));
-
-            // set timer to 30 min intervals after initial timer fires off which will likely not have been on a 30 min even interval
-            timer.Interval = (1800 * 1000);
+                synth.Speak(String.Format("The time is {0}", DateTime.Now.ToString("h:mm tt")));
+            }
         }
 
         private void Options(object sender, EventArgs e)
